9119a72d4abef9cb8ad419db2490dad24257361a
Novailoveyou <novailoveyoux@gmail.com>
2021-12-16 12:59:46 +0300

    basic cleanup

--- helpers/index.js
+++ helpers/index.js
@@ -1,19 +1,5 @@
-import getLastSixMonths from './getLastSixMonths'
-import getBestManagerResult from './getBestManagerResult'
-import fetchNotionData from './fetchNotionData'
-import getCurrentMonthNum from './getCurrentMonthNum'
 import getMonthFromNum from './getMonthFromNum'
 import isCurrentMonth from './isCurrentMonth'
-import sumMonths from './sumMonths'
 import toNumberWithSpaces from './toNumberWithSpaces'
 
-export {
-  getLastSixMonths,
-  getBestManagerResult,
-  fetchNotionData,
-  getCurrentMonthNum,
-  getMonthFromNum,
-  isCurrentMonth,
-  sumMonths,
-  toNumberWithSpaces
-}
+export { getMonthFromNum, isCurrentMonth, toNumberWithSpaces }
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,3 @@
-import {
-  fetchNotionData,
-  getLastSixMonths,
-  isCurrentMonth,
-  sumMonths
-} from '../helpers'
 import { PostonentsProvider } from 'postonents'
 import {
   HeaderLogo,
@@ -21,49 +15,7 @@ import {
   Footer
 } from '../components'
 
-const Home = ({ data }) => {
-  const dataThisMonth = data
-    .filter(item => isCurrentMonth(item.month) && item)
-    .sort((a, b) => b.result - a.result)
-  const lastSixMonths = getLastSixMonths()
-  const dataLastSixMonths = data.filter(item => {
-    let include = false
-    lastSixMonths.forEach(month => {
-      month.toLowerCase() === item.month.toLowerCase() && (include = true)
-    })
-    if (include) return item
-  })
-  const dataFromJuly = data.filter(item => {
-    const months = [
-      'Июль',
-      'Август',
-      'Сентябрь',
-      'Октябрь',
-      'Ноябрь',
-      'Декабрь'
-    ]
-
-    let include = false
-    months.forEach(month => {
-      month.toLowerCase() === item.month.toLowerCase() && (include = true)
-    })
-    if (include) return item
-  })
-
-  const twoMonthsInFromAugust = data.filter(item => {
-    const months = ['Август', 'Сентябрь']
-
-    let include = false
-    months.forEach(month => {
-      month.toLowerCase() === item.month.toLowerCase() && (include = true)
-    })
-    if (include) return item
-  })
-
-  console.log(data)
-  const twoMonthsInFromAugustSumUp = sumMonths(twoMonthsInFromAugust)
-  const dataFromJulySumUp = sumMonths(dataFromJuly)
-
+const Home = () => {
   return (
     <PostonentsProvider theme={{ typo: { fontFamily: 'Stem, sans-serif' } }}>
       <EmailWrapper>
@@ -78,16 +30,6 @@ const Home = ({ data }) => {
       </EmailWrapper>
     </PostonentsProvider>
   )
-}
-
-export async function getStaticProps(context) {
-  const data = await fetchNotionData()
-
-  return {
-    props: {
-      data
-    }
-  }
 }
 
 export default Home
